9038720e541516de50b8d04d73f52c7030cc99e0
Unknown <jvander2@ncsu.edu>
2018-02-28 14:53:10 -0500

    added functions code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCachedMatrix makes an object of type CachedMatrix that has 2 accessor functions
+## and two mutator function
 
-## Write a short comment describing this function
+## constructor for objects of thype cacheMatrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  
+  setSolve <- function(solve) m <<- solve
+  
+  getSolve <- function() m
+  
+  list(set = set, get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
 
 }
 
 
-## Write a short comment describing this function
+## returns inverted maxtrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getSolve()
+  if (!is.null(m)){
+    message("getting cached data")
+    return (m)
+  }
+  
+  data <- x$get()
+  m <- solve(x)
+  x$setSolve(m)
+  m
 }
